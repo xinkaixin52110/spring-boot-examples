1b0b2025110af4d9c69449c23f289b7b78f7b225
xinkaixin <123456>
2020-09-28 14:11:26 +0800

    1

---
+++ spring-boot-hello/src/main/java/com/neo/controller/IndexController.java
@@ -0,0 +1,24 @@
+package com.neo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+	private static final Logger _logger = LoggerFactory.getLogger(IndexController.class);
+	/**
+	 * 主页
+	 *
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping({"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("msg", "welcome you!");
+        return "index";
+    }
+}
